f4e11652f4f3fe329569c6c783ca5785fd15131b
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-01 19:56:43 -0300

    adicionado efeito fade in para telas maiores que 768px

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // efeitos ao carregar a página
 startEffects = () => {
+    
+    // efeitos ao carregar a página
     setTimeout(() => {
         document.querySelector("#header h1").classList.remove("is-loading");
         document.querySelector("#header p").classList.remove("is-loading");
         document.querySelector("#header img").classList.remove("is-loading");
+        
         let menuLinks = document.querySelectorAll("#menu a")
             for(link of menuLinks){
                 link.classList.remove("is-loading-links")
         }
     }, 500)
+
+    // efeito de fade in
+    setTimeout(() => {
+            document.querySelector("#page-background").classList.remove("page-background-is-loading");
+    }, 2000)
 }
 
 startEffects();
