092aee782f57f33948ff86f1747337d91a6c6c1e
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-01 18:20:28 -0300

    Modificado tempo para iniciar o efeito ao carregar página

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // efeitos ao carregar a página
 startEffects = () => {
-    document.querySelector("#header h1").classList.remove("is-loading");
-    document.querySelector("#header p").classList.remove("is-loading");
-    document.querySelector("#header img").classList.remove("is-loading");
     setTimeout(() => {
+        document.querySelector("#header h1").classList.remove("is-loading");
+        document.querySelector("#header p").classList.remove("is-loading");
+        document.querySelector("#header img").classList.remove("is-loading");
         let menuLinks = document.querySelectorAll("#menu a")
             for(link of menuLinks){
                 link.classList.remove("is-loading-links")
